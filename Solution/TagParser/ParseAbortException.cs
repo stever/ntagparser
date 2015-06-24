128ab3827faca89ad6eeeb1df8487fe63a719cfa
Steve Robertson <steve@playnode.net>
2015-06-24 19:43:51 +0100

    Removing two redundant classes

--- Solution/TagParser/ParseAbortException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace TagParser
-{
-    /// <summary>
-    /// This exception can be used to abort the tag parser.
-    /// </summary>
-    public class ParseAbortException : Exception { }
-}
